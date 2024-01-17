987da719a4b0a26d71d3e7c32d1d5bc705958659
minhnhut2306 <nhutchodiennguyen23062004@gmail.com>
2024-01-17 11:12:48 +0700

    thêm search

--- Components/explore/Explore.js
+++ Components/explore/Explore.js
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, TextInput, Image } from 'react-native';
+import React from 'react';
 
 const Explore = () => {
   return (
-    <View>
-      <Text>Explore</Text>
+    <View style={styles.container}>
+      <View style={styles.contaifind}>
+        <Text style={styles.findtext}>Find Products</Text>
+      </View>
+      <View style={styles.containersearch}>
+        <View style={styles.searchWrapper}>
+          <Image source={require('../../Image/search.png')} style={styles.searchIcon} />
+          <TextInput placeholder='Search Store' clearButtonMode='always' style={styles.search} />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Explore
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  findtext: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    fontFamily: 'Gilroy-Bold',
+    lineHeight: 20,
+  },
+  contaifind: {
+    width: 126,
+    height: 18,
+    marginTop: 56.93,
+  },
+  containersearch: {
+    width: '100%',
+    padding: 20,
+    marginTop: 26,
+  },
+  searchWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    height: 51,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+  },
+  searchIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+  },
+  search: {
+    flex: 1,
+  },
+});
+
+export default Explore;
