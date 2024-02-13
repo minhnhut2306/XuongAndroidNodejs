ff61e8c5ed869cd8f16dd935341f817d53554823
tranngoctu05 <130238498+tranngoctu05@users.noreply.github.com>
2024-02-13 22:10:08 +0700

    20%
    20%

--- Components/main/tabs/cart/Cart.js
+++ Components/main/tabs/cart/Cart.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, Image } from 'react-native'
 import React from 'react'
-
+import styles from './styles'
 const Cart = () => {
   return (
-    <View>
-      <Text>Cart</Text>
+    <View style={styles.container}>
+      <StatusBar
+        backgroundColor="rgba(255, 255, 255, 0)"
+        translucent={true}
+      />
+      <View style={styles.header}>
+        <Text style={styles.textCart}>My Cart</Text>
+      </View>
+      <View style={styles.content}>
+        <View style={styles.Line}>
+
+        </View>
+        <View style={styles.item}>
+          <Image
+            style={styles.imageItem}
+            source={require('../../../../assets/Image/cachua.png')} />
+            <Text style={styles.nameItem}>Bell Pepper Red</Text>
+            <Text style={styles.amoutItem}>1kg, Pice</Text>
+            <Image
+            style={styles.deleteItem}
+            source={require('../../../../assets/Image/delete.png')} />
+        </View>
+        <View style={styles.item}>
+          <Image
+            style={styles.imageItem}
+            source={require('../../../../assets/Image/cachua.png')} />
+        </View>
+      </View>
     </View>
   )
 }
